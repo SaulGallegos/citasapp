bce326672190ec7db466698d8b1d85964cab6a00
Saul Gallegos <saulladrondeguevara@gmail.com>
2024-02-09 16:27:27 -0600

    project week 1

---
+++ SPA/src/app/register/register.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  @Output() cancelRegister = new EventEmitter();
+  model: any= {};
+
+  constructor(private accountService: AccountService, private toastr: ToastrService){
+  };
+  NgOnInit(): void{
+  }
+
+  register(): void{
+    this.accountService.register(this.model).subscribe({
+      next: () => {
+        this.cancel();
+      },
+      error: error => {
+        this.toastr.error(error.error),
+        console.log(error)
+      }
+    });    
+  }
+  cancel(): void{
+    this.cancelRegister.emit(false);    
+  }
+
+}
